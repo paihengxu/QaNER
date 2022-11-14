c36b681b6d1b3341f6c08a9d6ffd7acb24406fc5
paihengxu <xupaiheng@hotmail.com>
2022-11-13 21:06:29 -0500

    update metrics (Label O)

--- qaner/metrics.py
+++ qaner/metrics.py
@@ -5,11 +5,22 @@ import numpy as np
 from qaner.data_utils import Span
 
 
+def find_sub_list(sl, l):
+    results = []
+    sll = len(sl)
+    for ind in (i for i, e in enumerate(l) if e == sl[0]):
+        if l[ind:ind + sll] == sl:
+            results.append((ind, ind + sll - 1))
+
+    return results
+
+
 # TODO: add metrics over label types
 def compute_metrics(
     spans_true_batch: List[Span],
     spans_pred_batch_top_1: List[List[Span]],
     prompt_mapper: Dict[str, str],
+    # context_list: List[str],
 ) -> Dict[str, float]:
     """
     Compute NER metrics.
@@ -18,7 +29,7 @@ def compute_metrics(
         spans_true_batch (List[Span]): targets.
         spans_pred_batch_top_1 (np.ndarray): predictions.
         prompt_mapper (Dict[str, str]): prompt mapper.
-
+        context_list:
     Returns:
         Dict[str, float]: metrics.
     """
@@ -33,9 +44,22 @@ def compute_metrics(
     confusion_matrix_true_denominator = np.zeros(len(entity_mapper))
     confusion_matrix_pred_denominator = np.zeros(len(entity_mapper))
 
-    for span_true, span_pred in zip(spans_true_batch, spans_pred_batch_top_1):
-        print(span_true)
-        print(span_pred)
+    # assert len(context_list) % (len(entity_mapper)-1) == 0, \
+    #     f"batch size: {len(context_list)}, # label: {len(entity_mapper)-1}:"
+
+    # batch_label = []
+    # batch_pred = []
+    for idx, (span_true, span_pred) in enumerate(zip(spans_true_batch, spans_pred_batch_top_1)):
+        # if idx % (len(entity_mapper)-1) == 0:
+        #     if idx != 0:
+        #         batch_label.append(tmp_label)
+        #         batch_pred.append(tmp_pred)
+        #     else:
+        #         context = context_list[idx]
+        #         context_len = len(context.split(' '))
+        #         tmp_label = ['O'] * context_len
+        #         tmp_pred = ['O'] * context_len
+
         span_pred = span_pred[0]  # type: ignore
 
         i = entity_mapper[span_true.label]
@@ -70,10 +94,11 @@ def compute_metrics(
         * recall_per_entity_type
         / (precision_per_entity_type + recall_per_entity_type)
     )
-
     metrics["accuracy"] = accuracy
 
     for label_tag, idx in entity_mapper.items():
+        if idx == 0:
+            continue
         metrics[f"precision_tag_{label_tag}"] = precision_per_entity_type[idx]
         metrics[f"recall_tag_{label_tag}"] = recall_per_entity_type[idx]
         metrics[f"f1_tag_{label_tag}"] = f1_per_entity_type[idx]
@@ -81,22 +106,27 @@ def compute_metrics(
     # TODO: add micro average
 
     # macro average
-    metrics["precision_macro"] = precision_per_entity_type.mean()
-    metrics["recall_macro"] = recall_per_entity_type.mean()
-    metrics["f1_macro"] = f1_per_entity_type.mean()
+    metrics["precision_macro"] = precision_per_entity_type[1:].mean()
+    metrics["recall_macro"] = recall_per_entity_type[1:].mean()
+    metrics["f1_macro"] = f1_per_entity_type[1:].mean()
 
     # weighted average
-    metrics["precision_weighted"] = np.average(
-        precision_per_entity_type,
-        weights=confusion_matrix_true_denominator,
-    )
-    metrics["recall_weighted"] = np.average(
-        recall_per_entity_type,
-        weights=confusion_matrix_true_denominator,
-    )
-    metrics["f1_weighted"] = np.average(
-        f1_per_entity_type,
-        weights=confusion_matrix_true_denominator,
-    )
+    if np.sum(confusion_matrix_true_denominator[1:]) == 0:
+        metrics["precision_weighted"] = 0
+        metrics["recall_weighted"] = 0
+        metrics["f1_weighted"] = 0
+    else:
+        metrics["precision_weighted"] = np.average(
+            precision_per_entity_type[1:],
+            weights=confusion_matrix_true_denominator[1:],
+        )
+        metrics["recall_weighted"] = np.average(
+            recall_per_entity_type[1:],
+            weights=confusion_matrix_true_denominator[1:],
+        )
+        metrics["f1_weighted"] = np.average(
+            f1_per_entity_type[1:],
+            weights=confusion_matrix_true_denominator[1:],
+        )
 
     return metrics
